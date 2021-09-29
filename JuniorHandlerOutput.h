76821fd373ef34de266390b51c1726317aaaadc9
AnatolFil <tolyanfilatov@mail.ru>
2021-09-29 22:18:52 +0300

    Added some comments

--- JuniorHandlerOutput.h
+++ JuniorHandlerOutput.h
@@ -16,9 +16,21 @@ class JuniorHandlerOutput
 		static void ShowYearOfWork(Junior Jun);
 		static void ShowPhone(Junior Jun);
 		static void Show(Junior Jun);
+		//Juns - input data
+		//OUTJuns - result of working
+		//OUTCount - amount of elements in OUTJuns
 		static void FindBySpecialization(Junior *Juns, int count, _TCHAR Specialization[], Junior* OUTJuns, int *OUTCount);
+		//Juns - input data
+		//OUTJuns - result of working
+		//OUTCount - amount of elements in OUTJuns
 		static void FindByYearOfWork(Junior* Juns, int count, int YearOfWork, Junior* OUTJuns, int* OUTCount);
+		//Juns - input data
+		//OUTJuns - result of working
+		//OUTCount - amount of elements in OUTJuns
 		static void FindAfterBirthYear(Junior* Juns, int count, int BithYear, Junior* OUTJuns, int* OUTCount);
 		static void SortByBirthYear(Junior* Juns, int count);
+		//Sort using sort from algorithm
+		//Last param is pointer on comparator for sort function
+		//Param Juns is input data and after processing will contain Result of lexigraph sorting 
 		static void SortByBirthSpecialization(Junior* Juns, int count, bool (*f)(Junior, Junior));
 };
